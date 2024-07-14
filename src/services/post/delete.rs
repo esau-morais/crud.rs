ad965651350a202dfe8b24ce7b4ddf7aee6aca8e
esau-morais <esaumorais7@gmail.com>
2024-07-14 16:06:55 -0300

    test: add delete route tests

--- src/services/post/delete.rs
+++ src/services/post/delete.rs
@@ -33,3 +33,61 @@ pub async fn delete_post(path: web::Path<i32>) -> HttpResponse {
 
     HttpResponse::NoContent().finish()
 }
+
+#[cfg(test)]
+mod tests {
+    use actix_web::{http::StatusCode, test, App};
+    use diesel::{query_dsl::methods::FilterDsl, ExpressionMethods, RunQueryDsl};
+
+    use crate::{
+        db,
+        routes::post::delete_post_route,
+        schema::posts::dsl::{id, posts},
+        shared::post::shared_create_post,
+    };
+
+    #[actix_web::test]
+    async fn test_delete_post_by_id() {
+        let app = test::init_service(App::new().service(delete_post_route)).await;
+
+        let recent_created_post_id = shared_create_post().await.id;
+        let req = test::TestRequest::delete()
+            .uri(&format!("/posts/{}", recent_created_post_id))
+            .to_request();
+        let res = test::call_service(&app, req).await;
+        assert!(res.status().is_success());
+        assert_eq!(res.status(), StatusCode::NO_CONTENT);
+
+        // cleanup after test is done
+        diesel::delete(posts.filter(id.eq(recent_created_post_id)))
+            .execute(&mut db::init())
+            .expect("failed to clean up posts table");
+    }
+
+    #[actix_web::test]
+    async fn test_try_delete_unexistent_post() {
+        let app = test::init_service(App::new().service(delete_post_route)).await;
+
+        let recent_created_post_id = shared_create_post().await.id;
+
+        // cleanup before test is done to test case in which post does not exist
+        diesel::delete(posts.filter(id.eq(recent_created_post_id)))
+            .execute(&mut db::init())
+            .expect("failed to clean up posts table");
+
+        let req = test::TestRequest::delete()
+            .uri(&format!("/posts/{}", recent_created_post_id))
+            .to_request();
+        let res = test::call_service(&app, req).await;
+        assert!(res.status().is_client_error());
+        assert_eq!(res.status(), StatusCode::NOT_FOUND);
+
+        let body: serde_json::Value = test::read_body_json(res).await;
+
+        assert_eq!(body["status"], "error");
+        assert_eq!(
+            body["message"],
+            format!("post with ID {} not found", recent_created_post_id),
+        );
+    }
+}
